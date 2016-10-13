22f62b66833be5a59f914b7bbd6062f0d1bb46df
DanielMaj94 <bft2012-03@cloud.bs-hassfurt.de>
2016-10-13 12:45:03 +0200

    NeueSicherung
    Sicherung

---
+++ Krankenmeldung/Krankenmeldung/Dokument.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Krankmeldung 
+{
+    class Dokument : INotifyPropertyChanged
+    {
+        string pfad;
+        string beschreibung;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Beschreibung
+        {
+            get { return beschreibung; }
+            set { 
+                beschreibung = value;
+                NotifyPropertyChanged("Beschreibung");
+            }
+        }
+
+        public string Pfad
+        {
+            get { return pfad; }
+            set {
+                pfad = value;
+                NotifyPropertyChanged("Pfad");
+            }
+        }
+
+        public Dokument(string _beschreibung)
+        {
+            this.beschreibung = _beschreibung;
+        }
+
+        private void NotifyPropertyChanged(string info)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(info));
+        }
+
+
+
+    }
+}
---
+++ Krankenmeldung/Krankenmeldung/Login.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Krankmeldung
+{
+    class Login : INotifyPropertyChanged
+    {
+        string benutzername;
+        string passwort;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Benutzername
+        {
+            get { return benutzername; }
+            set {
+                benutzername = value;
+                NotifyPropertyChanged("Benutzername");
+            }
+        }
+        
+        public string Passwort
+        {
+            get { return passwort; }
+            set {
+                passwort = value;
+                NotifyPropertyChanged("Passwort");
+            }
+        }
+
+        public Login()
+        {
+
+        }
+
+        private void NotifyPropertyChanged(string info)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(info));
+        }
+
+    }
+}
